0bb782a16bf6a03bdf626284dcb240544966eee4
TeamMeng <862915265@qq.com>
2024-11-13 22:19:16 +0800

    feature: init thr project

---
+++ chat/chat_server/src/handlers/workspace.rs
@@ -0,0 +1,12 @@
+use crate::{AppError, AppState};
+use axum::{extract::State, response::IntoResponse, Extension, Json};
+use chat_core::User;
+
+/// List all users in the workspace.
+pub(crate) async fn list_chat_users_handler(
+    Extension(user): Extension<User>,
+    State(state): State<AppState>,
+) -> Result<impl IntoResponse, AppError> {
+    let users = state.fetch_chat_users(user.ws_id as _).await?;
+    Ok(Json(users))
+}
